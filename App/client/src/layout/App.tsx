4d1fb8846c2085a23e6d27d91e81b7771d4c243c
ni2be <m.j.scholzen@gmail.com>
2022-09-28 16:43:00 +0200

    under construction component

--- App/client/src/layout/App.tsx
+++ App/client/src/layout/App.tsx
@@ -6,13 +6,14 @@ import SideBar from "../components/SideBar/SideBar";
 import ProjectList from "../components/Projects/ProjectList";
 import { Grid } from "semantic-ui-react";
 import About from "../components/About/About";
+import UnderConstruction from "../components/UnderConstruction/UnderConstruction";
 
 const App = () => {
   const sideBarWidth = '12rem';
 
   return (
     <div className="App dark-theme" role="application">
-
+    <UnderConstruction/>
       <AnimatedBackground />
 
       <SideBar sideBarWidth={sideBarWidth} />
