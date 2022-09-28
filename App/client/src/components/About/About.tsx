39938b50f7f3cf950c1d30610461405f179d1a43
ni2be <m.j.scholzen@gmail.com>
2022-09-28 13:51:40 +0200

    about frontend

---
+++ App/client/src/components/About/About.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import agent from "../../api/agent";
+import Skills from "../Skills/Skills";
+import './About.scss'
+
+function About() {
+
+    return (
+        <div className="about">
+            <Grid columns="2" >
+                <Grid.Column >
+                    <div className="aboutTextBox" >
+                        <h1>Hi, I’m Jordan,</h1>
+                        <p>
+                            I’m a full stack developer and consultant from Germany. Currently I work for Bosch Access Systems as software engineer, product owner and product security manager.
+                        </p>
+                        <h1>How can I help you?</h1>
+                        <ul>
+                            <li>I have a passion for the whole full stack. From micro-services in .Net over web apps in React to CI/CD pipelines, I’ve got your back!</li>
+                            <li>Windows installer in WiX Toolset – no problem!</li>
+                            <li>Shader programming and ECS development in Unity, let’s go!</li>
+                            <li>Anything else? Don't hesitate to ask, I'm sure we’ll find a solution!</li>
+                            <li>I should stop shouting?! Alight!!!</li>
+                        </ul>
+                        <h1>How to reach me?</h1>
+                        <p>
+                            Message me on LinkedIn – write a public Issue on GitHub <br />
+                            Or let me take you to the <a href="/contact">contact page</a>
+                        </p>
+                    </div >
+                </Grid.Column>
+                <Grid.Column>
+                    <div className="aboutSkills">
+                        <Skills />
+                    </div>
+                </Grid.Column>
+            </Grid>
+        </div>
+    )
+}
+
+export default About;
--- App/client/src/components/Logo/Logo.tsx
+++ App/client/src/components/Logo/Logo.tsx
@@ -1,16 +1,35 @@
 import React from "react"
-import { Image } from "semantic-ui-react"
 import "./Logo.scss";
 
 interface IProps {
     text?: string;
 }
 
-const Logo: React.FC<IProps> = ({text}) => {
+const Logo: React.FC<IProps> = ({ text }) => {
 
     return (
         <div className="logo">
-            <Image src={'assets/logo/logo.svg'} />
+            <svg viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="logo" clipPath="url(#clip0_5_64)">
+                    <g id="rect">
+                        <path id="rect-4" 
+                        d="M33.1406 17.0591L17.0591 0.977661L0.977661 17.0591L17.0591 33.1406L33.1406 17.0591Z" 
+                        strokeWidth="1.38262" />
+                        <path id="rect-3" 
+                        d="M29.7802 17.0592L17.0591 4.33813L4.33807 17.0592L17.0591 29.7803L29.7802 17.0592Z" 
+                        strokeWidth="1.09371" />
+                        <path id="rect-2" 
+                        d="M27.0732 17.0591L17.0591 7.04504L7.04507 17.0591L17.0591 27.0732L27.0732 17.0591Z" 
+                        strokeWidth="0.860969" />
+                        <path id="rect-1" 
+                        d="M22.5341 17.0592L17.0591 11.5842L11.5842 17.0592L17.0591 22.5341L22.5341 17.0592Z" 
+                        strokeWidth="0.470714" />
+                        <path id="rect-0" 
+                        d="M24.6462 17.0592L17.0591 9.47205L9.47204 17.0592L17.0591 24.6463L24.6462 17.0592Z" 
+                        strokeWidth="0.652309" />
+                    </g>
+                </g>
+            </svg>
             <p>{text}</p>
         </div>
     )
