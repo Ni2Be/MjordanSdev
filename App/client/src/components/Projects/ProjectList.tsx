3cc80579ee528e2ec2d7e5b22fd7f7b31b9759ea
ni2be <m.j.scholzen@gmail.com>
2022-10-15 13:35:22 +0200

    clean up

--- App/client/src/components/Projects/ProjectList.tsx
+++ App/client/src/components/Projects/ProjectList.tsx
@@ -38,7 +38,7 @@ function ProjectList() {
 
     return (
         <div className="projectList" >
-            {loading && Array(3).fill(0).map((p, i) => <ProjectPreviewPlaceholder key={i}/>)}
+            {loading && Array(3).fill(0).map((_, i) => <ProjectPreviewPlaceholder key={i}/>)}
             {
                 projects?.map(project => {
                     return (
