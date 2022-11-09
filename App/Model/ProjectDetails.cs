fd29680c8298400203a910a338ecaa4198939e0d
M. Jordan Scholzen <m.j.scholzen@gmail.com>
2022-11-09 18:09:52 +0100

    project id strings (#55)

--- App/Model/ProjectDetails.cs
+++ App/Model/ProjectDetails.cs
@@ -3,7 +3,7 @@ public class ProjectDetails : IImageOwner
 {
     public Guid Id { get; set; }
     public Project Project { get; set; }
-    public Guid ProjectId { get; set; }
+    public string ProjectId { get; set; }
     public bool DefaultDetails { get; set; }
     public string Description { get; set; }
     public string BulletPoints { get; set; }
